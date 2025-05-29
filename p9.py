1d1071aad1ef25d6840ff869ec38f021c8470f96
Adam Månsson <adam.s.mansson@gmail.com>
2025-05-29 22:05:28 +0200

    update p9

--- p9.py
+++ p9.py
@@ -1,10 +1,8 @@
-found = None
-n = 1
 for k in range(1, 100):
     for m in range(2, 100, 2):
-        a = k * (m**2 - n**2)
-        b = k * (2 * m * n)
-        c = k * (m**2 + n**2)
+        a = k * (m**2 - 1)
+        b = k * (2 * m)
+        c = k * (m**2 + 1)
         if a + b + c == 1000:
             print(a * b * c)
             exit()

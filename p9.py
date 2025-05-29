8a800acbd5d3982067b4678803d1b097915951fa
Adam Månsson <adam.s.mansson@gmail.com>
2025-05-29 16:07:17 +0200

    use black

--- p9.py
+++ p9.py
@@ -3,7 +3,7 @@ n = 1
 for k in range(1, 100):
     for m in range(2, 100, 2):
         a = k * (m**2 - n**2)
-        b = k * (2*m*n)
+        b = k * (2 * m * n)
         c = k * (m**2 + n**2)
         if a + b + c == 1000:
             print(a * b * c)

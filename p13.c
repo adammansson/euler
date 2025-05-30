eaba9384cdfc2c90cb2b169f06a5d80e81b05dde
Adam Månsson <adam.s.mansson@gmail.com>
2025-05-30 15:20:28 +0200

    add p13

---
+++ p13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+  FILE *fp;
+  char b[5101];
+  int i;
+  int j;
+  unsigned short s;
+  unsigned short c;
+  char a[11];
+  fp = fopen("p13.in", "r");
+  fread(b, 1, 5100, fp);
+  fclose(fp);
+  b[5100] = '\0';
+  c = 0;
+  for (i = 49; i >= 0; --i) {
+    s = c;
+    for (j = 0; j < 100; ++j)
+      s += b[i + 51*j] - '0';
+    c = s / 10;
+    if (i < 8)
+      a[i + 2] = (s % 10) + '0';
+  }
+  a[0] = (c / 10) + '0';
+  a[1] = (c % 10) + '0';
+  a[10] = '\0';
+  printf("%s\n", a);
+}
---
+++ p13.py
@@ -0,0 +1,3 @@
+with open("p13.in", "r") as f:
+    print(sum([int(n) for n in f.readlines()]) // 10**42)
+
